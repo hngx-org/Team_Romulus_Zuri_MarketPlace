a5a7f6be604153866d7325cefc5760db6a936512
John Diginee <Johndbizz@gmail.com>
2023-10-07 23:36:02 +0100

    Feat: Add recently viewed products

--- core/settings.py
+++ core/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'MarketPlace'
+    'MarketPlace',
+    'drf_yasg',
+    'product_view',
 ]
 
 MIDDLEWARE = [
---
+++ product_view/views.py
@@ -0,0 +1,29 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+
+from .models import ProductView
+from .serializers import ProductViewSerializer
+
+class GetLastViewedProducts(APIView):
+    @staticmethod
+    def get(self, request, user_id):
+        """Check if the user exists."""
+        try:
+            user = User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        """Get the list of products that the user has recently viewed."""
+        product_views = ProductView.objects.filter(user_id=user_id).order_by('-timestamp')
+
+        """If the user has not viewed any products recently, return an empty list."""
+        if not product_views.exists():
+            return Response([])
+
+        """Serialize the list of product views."""
+        serializer = ProductViewSerializer(product_views, many=True)
+
+        """Return the serialized list of product views."""
+        return Response(serializer.data)
