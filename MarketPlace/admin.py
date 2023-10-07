e1b1f68ba51a19308c9796224a923f655bd227cd
Code bee <codebee345@outlook.com>
2023-10-07 23:18:26 +0100

    Added product_category and product_image models

--- MarketPlace/admin.py
+++ MarketPlace/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import ProductCategory, ProductImage
 
 # Register your models here.
+admin.site.register(ProductImage)
+admin.site.register(ProductCategory)
