b9138333254fba78a6a97bfe2aa6bcf82f62ef80
Mahlet Seifu <akotet2123@gmail.com>
2023-10-08 03:23:00 +0300

    filters the products based on certain parameters

--- MarketPlace/urls.py
+++ MarketPlace/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from  .views import SimilarProductView
+from  .views import SimilarProductView, FilterProductView
 
 urlpatterns = [
     path('similar_products/<uuid:product_id>/', SimilarProductView.as_view(), name='similar-products'),
+    path('products/', FilterProductView.as_view(), name='filter_products'),
 ]
