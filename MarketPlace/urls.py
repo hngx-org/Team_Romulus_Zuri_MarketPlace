b6d66ff9549949eb4ce8b511cb8e4af72cf8976c
Breeze Concept <breezeconcept3@gmail.com>
2023-10-08 12:35:21 +0100

    Added product recommendation logic

--- MarketPlace/urls.py
+++ MarketPlace/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from  .views import SimilarProductView, FilterProductView
+from  .views import SimilarProductView, FilterProductView, RecommendationView
 
 urlpatterns = [
     path('similar_products/<uuid:product_id>/', SimilarProductView.as_view(), name='similar-products'),
     path('products/', FilterProductView.as_view(), name='filter_products'),
+    path('recommendations/<int:user_id>/', RecommendationView.as_view(), name='recommendations'),
 ]
