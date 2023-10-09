6bd45a5da7d713cf07a1a29dce4bd9987dce9c2d
wambuiwambui <tnjoki11@gmail.com>
2023-10-09 21:58:36 +0300

    Remove wishlistitem duplicate model

--- MarketPlace/models.py
+++ MarketPlace/models.py
@@ -245,11 +245,3 @@ class UserProductInteraction(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     interaction_type = models.CharField(max_length=10)  # e.g., "viewed," "purchased"
     timestamp = models.DateTimeField(auto_now_add=True)
-
-class WishListItem(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    products = models.ManyToManyField(Product)
-    date_added = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.user.username}'s Wishlist Item"
--- MarketPlace/serializers.py
+++ MarketPlace/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Product, Wishlist, WishListItem
+from .models import Product, Wishlist
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -11,9 +11,3 @@ class WishlistSerializer(serializers.ModelSerializer):
     class Meta:
         model = Wishlist
         fields = '__all__'
-
-class WishListItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = WishListItem
-        fields = '__all__'
-
