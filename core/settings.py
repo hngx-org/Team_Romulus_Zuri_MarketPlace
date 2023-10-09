27e6262682b30f7df6732e3660a03faf81aa9e6d
John Diginee <Johndbizz@gmail.com>
2023-10-09 11:57:33 +0100

    Fix: Fix bug in settings.py

--- core/settings.py
+++ core/settings.py
@@ -91,6 +91,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASE_ROUTERS = ['api.db_routers.DefaultDBRouter', 'api.db_routers.SharedDBRouter']
 
 DATABASES = {
+        'shared_db': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('DB_NAME'),
+            'HOST': os.environ.get('DB_HOST'),
+            'PORT': os.environ.get('DB_PORT'),
+            'USER': os.environ.get('DB_USER'),
+            'PASSWORD': os.environ.get('DB_PASSWORD'),
+        },
         "default": {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
