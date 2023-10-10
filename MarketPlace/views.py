174eaf34e0def20003d6c5d3f9ad9a5165220fc0
Dolapo24 <adedolapovictoria927@gmail.com>
2023-10-10 11:07:56 +0100

    similar product recommendation

--- MarketPlace/views.py
+++ MarketPlace/views.py
@@ -21,17 +21,16 @@ class Status(APIView):
 
 class SimilarProductView(APIView):
     @staticmethod
-    def get(request, category_id):
+    def get(request, product_id):
         try:
-            current_product = Product.objects.get(id=category_id)
-            print(f'current_product: {current_product.id}')
+            current_product = Product.objects.get(id=product_id)
         except Product.DoesNotExist:
             return Response({'error': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
-        
-        similar_products = Product.objects.filter(category_id=current_product.category_id).exclude(id=category_id)
-        similar_products = sample(list(similar_products), 4)
 
-        serializer = ProductSerializer(similar_products, many=True)
+        similar_products = Product.objects.filter(category_id=current_product.category_id).exclude(id=product_id)
+        recommended_products = similar_products[:4]
+
+        serializer = ProductSerializer(recommended_products, many=True)
 
         return Response({'products': serializer.data}, status=status.HTTP_200_OK)
 
@@ -58,6 +57,7 @@ class FilterProductView(APIView):
 
         return Response({'products': serializer.data}, status=status.HTTP_200_OK)
 
+
 class ProductListByCategoryView(APIView):
     serializer_class = ProductSerializer
 
@@ -66,7 +66,7 @@ class ProductListByCategoryView(APIView):
 
         try:
             category = ProductCategory.objects.get(name=categories)
-            products = Product.objects.filter(category=category).order_by(sort_by)
+            products = Product.objects.filter(category_id=category).order_by(sort_by)
             
             
             if not products.exists():
