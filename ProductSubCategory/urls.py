80b528fa99c520834a10d03d44079f8db85e3473
Femi-ID <104504319+Femi-ID@users.noreply.github.com>
2023-10-12 12:37:52 +0100

    Feat: Create ProductListByCategoryView

--- ProductSubCategory/urls.py
+++ ProductSubCategory/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import GetCategoryNames, GetImage, GetProductsSubCategory
 
 urlpatterns = [
-        path('categoryNames', GetCategoryNames.as_view(), name='category-names')
-        path('image/<imageId>', GetImage.as_view(), name='images')
-        path('products/<str: categoryName>/<str: subCategoryName>', GetProductsSubCategory.as_view(), name='product-sub-category')
+        path('categoryNames', GetCategoryNames.as_view(), name='category-names'),
+        path('image/<imageId>', GetImage.as_view(), name='images'),
+        path('products/<str: categoryName>/<str: subCategoryName>', GetProductsSubCategory.as_view(), name='product-sub-category'),
 
 ]
