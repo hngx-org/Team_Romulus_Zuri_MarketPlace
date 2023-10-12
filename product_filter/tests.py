dc29fd1461b7d8fccdcbba55e33b32ce0d7fba7b
Femi-ID <104504319+Femi-ID@users.noreply.github.com>
2023-10-12 10:36:08 +0100

    Create unit-test for FilterProductView

--- product_filter/tests.py
+++ product_filter/tests.py
@@ -1,3 +1,61 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from MarketPlace.models import Shop, ProductCategory, ProductSubCategory, Product
+from django.urls import reverse
+from rest_framework import status
+from typing import OrderedDict
+from .serializers import ProductSerializer
 
-# Create your tests here.
+
+class TestFilterProductView(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        # To create sample categories
+        self.category1 = ProductCategory.objects.create(name='Electronics')
+        self.category2 = ProductCategory.objects.create(name='Online_Tutoring')
+
+        # To create sample products
+        self.product1 = Product.objects.create(name='product1',
+                                               description='Product1 description', quantity=10,
+                                               category=self.category1, price=1000.00, discount_price=900.00,
+                                               tax=50.00, is_published=True, currency='Naira')
+        self.product2 = Product.objects.create(name='product2',
+                                               description='Product2 description device', quantity=15,
+                                               category=self.category1, price=1500.00, discount_price=1350.00,
+                                               tax=75.00, is_published=True, currency='Naira')
+        self.product3 = Product.objects.create(name='product3',
+                                               description='Product3 description', quantity=25,
+                                               category=self.category2, price=1300.00, discount_price=1250.00,
+                                               tax=65.00, is_published=True, currency='Naira')
+
+    def test_filter_products(self):
+        # To create a query parameter with keywords, category and discount
+        query_params = {
+            'category': self.category2.id,
+            'keywords': 'device',
+            'discount': '1300.00'  # filter products with discount <= 1300
+        }
+
+        # GET request to FilterProductView endpoint with query parameters as filters
+        url = reverse('filter_products')
+        response = self.client.get(url, data={'ordering': 'name'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print(response.data)
+
+        # To verify that the products returned match at least one of the filter criteria(keywords):
+        filtered_products = response.json()
+        for product in filtered_products['products']:
+            self.assertTrue(
+                any(q.lower() in product['name'].lower() or product['description'].lower() for q in query_params['keywords'].split(','))
+            )
+
+        # To verify that the products returned matches at least one of the filter criteria(category):
+        for product in filtered_products['products']:
+            if product['category_id'] == query_params['category']:
+                self.assertTrue(product['category_id'], query_params['category'])
+
+        for product in filtered_products['products']:
+            if product['discount_price'] <= query_params['discount']:
+                self.assertLessEqual(product['discount_price'], query_params['discount'])
+                self.assertEqual(len(response.data['products']), 2)
+# ----------------------------------------------------------------------------------------------------------#
