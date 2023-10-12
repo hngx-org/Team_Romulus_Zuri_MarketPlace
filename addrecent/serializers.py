92ddc9e0fbcb97e96bbc781109b1792319d4a5d2
Code bee <codebee345@outlook.com>
2023-10-12 15:22:20 +0100

    added get product by id endpont

--- addrecent/serializers.py
+++ addrecent/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from MarketPlace.models import UserProductInteraction
+from MarketPlace.models import Product
 
 class UserProductInteractionSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProductInteraction
+        fields = '__all__'
+
+class ProductItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
         fields = '__all__'
