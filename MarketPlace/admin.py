606f8f2397c113985b82632c60b40ee7ac97510c
bukeme <basseyukemme@gmail.com>
2023-10-12 05:26:11 +0100

    registered all models in django admin

--- MarketPlace/admin.py
+++ MarketPlace/admin.py
@@ -1,5 +1,11 @@
-# from django.contrib import admin
-# from MarketPlace.models import Product
+from django.contrib import admin
+from MarketPlace.models import *
+import django.apps
 
-# # Register your models here.
-# admin.site.register(Product)
+models = django.apps.apps.get_models()
+
+for model in models:
+	try:
+		admin.site.register(model)
+	except:
+		pass
