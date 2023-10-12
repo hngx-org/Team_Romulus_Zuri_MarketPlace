eacba0180083c56295da7ca90213668ad30ef5a9
Mahlet Seifu <akotet2123@gmail.com>
2023-10-12 04:11:34 +0300

    my filter app

--- MarketPlace/admin.py
+++ MarketPlace/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from .models import ProductCategory, ProductImage, Product, Shop, Wishlist, User
+from .models import ProductCategory, ProductImage, Product, Shop, Wishlist, User, ProductSubCategory, UserProductRating
 
 # Register your models here.
 admin.site.register(ProductImage)
 admin.site.register(ProductCategory)
+admin.site.register(ProductSubCategory)
+admin.site.register(UserProductRating)
 admin.site.register(Product)
 admin.site.register(Shop)
 admin.site.register(Wishlist)
