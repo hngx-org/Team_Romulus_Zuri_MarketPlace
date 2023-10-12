389eed47e379e60b7370c5f52453b750a9af0fd4
Stoney <chisomd90@gmail.com>
2023-10-12 23:48:57 +0100

    added authentification delete_wishlist logic

--- delete_wishlist/tests.py
+++ delete_wishlist/tests.py
@@ -1,29 +1,46 @@
-from django.test import TestCase, override_settings
-from django.contrib.auth.models import User
-from rest_framework.test import APIClient
+# delete_wishlist/tests.py
+from django.urls import reverse
+from rest_framework.test import APITestCase
 from rest_framework import status
-from .models import Wishlist
+from django.contrib.auth.models import User
+from MarketPlace.models import Product, Wishlist
 
-@override_settings(DATABASES={'default': {'ENGINE': 'django.db.backends.sqlite3', 'NAME': ':memory:'}})
-class WishlistAPITest(TestCase):
-    databases = {'default'}  # Add this line to specify the database alias
-
+class DeleteFromWishlistTestCase(APITestCase):
     def setUp(self):
-        self.user = User.objects.create_user(username='testuser', password='testpassword')
-        self.client = APIClient()
-        self.client.login(username='testuser', password='testpassword')
-        self.product_id = 1
+        # Create a user
+        self.user = User.objects.create_user(username="testuser", password="testpassword")
 
-        # Create a wishlist item for the user
-        Wishlist.objects.create(user=self.user, product_id=self.product_id)
+        # Create a product
+        self.product = Product.objects.create(id="your_product_id", name="Test Product", description="Product Description", quantity=10, price=100.0)
+
+        # Create a wishlist item
+        self.wishlist_item = Wishlist.objects.create(user=self.user, product=self.product)
+
+        # Authenticate the user
+        self.client.force_authenticate(user=self.user)
 
     def test_delete_product_from_wishlist(self):
-        response = self.client.delete(f'/api/wishlist/{self.product_id}/')
+        # Ensure the product exists in the user's wishlist
+        self.assertEqual(Wishlist.objects.filter(user=self.user, product=self.product).count(), 1)
+
+        # Send a DELETE request to remove the product from the wishlist
+        url = reverse('delete_from_wishlist', args=[str(self.product.id)])
+        response = self.client.delete(url)
+
+        # Check if the response status code is 204 (No Content)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
-    def test_delete_product_not_in_wishlist(self):
-        response = self.client.delete('/api/wishlist/999/')  # A product that doesn't exist
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-        self.assertEqual(response.data['detail'], 'Product not in the wishlist')
+        # Ensure the product is no longer in the wishlist
+        self.assertEqual(Wishlist.objects.filter(user=self.user, product=self.product).count(), 0)
 
-    # Add more test cases for authentication, error handling, etc.
+    def test_delete_nonexistent_product_from_wishlist(self):
+        # Send a DELETE request to remove a non-existent product from the wishlist
+        non_existent_product_id = "non_existent_product_id"
+        url = reverse('delete_from_wishlist', args=[non_existent_product_id])
+        response = self.client.delete(url)
+
+        # Check if the response status code is 404 (Not Found)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+        # Ensure the wishlist is not modified
+        self.assertEqual(Wishlist.objects.filter(user=self.user).count(), 1)
--- delete_wishlist/views.py
+++ delete_wishlist/views.py
@@ -1,20 +1,58 @@
 from rest_framework import status
-from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from MarketPlace.models import Wishlist
-from .serializers import WishlistSerializer
+from rest_framework.exceptions import ValidationError
+import uuid
+from .models import Wishlist
+
 
 @api_view(['DELETE'])
 @permission_classes([IsAuthenticated])
-def delete_product_from_wishlist(request, product_id):
-    # Check if the product is in the user's wishlist
+def delete_from_wishlist(request, product_id):
     try:
+        # Check if the product_id is a valid UUID
+        uuid.UUID(str(product_id))
+    except ValueError:
+        raise ValidationError('Invalid UUID format for product_id')
+
+    try:
+        # Check if the product exists in the wishlist of the logged-in user
         wishlist_item = Wishlist.objects.get(user=request.user, product_id=product_id)
     except Wishlist.DoesNotExist:
-        return Response({'detail': 'Product not in the wishlist'}, status=status.HTTP_404_NOT_FOUND)
+        return Response({'message': 'Product not found in your wishlist.'}, status=status.HTTP_NOT_FOUND)
 
     # Delete the product from the wishlist
     wishlist_item.delete()
 
-    return Response({'detail': 'Product removed from the wishlist'}, status=status.HTTP_204_NO_CONTENT)
+    return Response({'message': 'Product removed from your wishlist.'}, status=status.HTTP_204_NO_CONTENT)
+
+# from rest_framework import status
+# from rest_framework.decorators import api_view, permission_classes
+# from rest_framework.response import Response
+# from rest_framework.permissions import IsAuthenticated
+# from rest_framework.exceptions import ValidationError
+# import uuid
+# from .models import Wishlist
+# from .decorators import skip_authentication  # Import the skip_authentication decorator
+
+# # Apply the skip_authentication decorator to the view
+# @api_view(['DELETE'])
+# @skip_authentication
+# def delete_from_wishlist(request, product_id):
+#     try:
+#         # Check if the product_id is a valid UUID
+#         uuid.UUID(str(product_id))
+#     except ValueError:
+#         raise ValidationError('Invalid UUID format for product_id')
+
+#     try:
+#         # Check if the product exists in the wishlist of the logged-in user
+#         wishlist_item = Wishlist.objects.get(user=request.user, product_id=product_id)
+#     except Wishlist.DoesNotExist:
+#         return Response({'message': 'Product not found in your wishlist.'}, status=status.HTTP_NOT_FOUND)
+
+#     # Delete the product from the wishlist
+#     wishlist_item.delete()
+
+#     return Response({'message': 'Product removed from your wishlist.'}, status=status.HTTP_204_NO_CONTENT)
