4a40d64bdcd8dc92261dcec53c02ee565f2e3312
Mahlet Seifu <akotet2123@gmail.com>
2023-10-12 09:40:14 +0300

    product filter feature api

--- MarketPlace/admin.py
+++ MarketPlace/admin.py
@@ -2,10 +2,6 @@ from django.contrib import admin
 from MarketPlace.models import *
 import django.apps
 
-<<<<<<< HEAD
-# Register your models here.
-admin.site.register(Product)
-=======
 models = django.apps.apps.get_models()
 
 for model in models:
@@ -13,4 +9,3 @@ for model in models:
 		admin.site.register(model)
 	except:
 		pass
->>>>>>> 92ec6e7eb7d506979780363fecb52787a30681e3
