75f6b899f5b86481045fb54e919e3fb13c6be20c
Breeze Concept <breezeconcept3@gmail.com>
2023-10-14 00:02:12 +0100

    modified core again

--- core/settings.py
+++ core/settings.py
@@ -191,6 +191,7 @@ REST_FRAMEWORK = {
     ],
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     # 'PAGE_SIZE': 5
+    
 }
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
