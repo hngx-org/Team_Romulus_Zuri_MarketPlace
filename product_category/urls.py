9cdae70cfc46529a68ebafeb1fc89edc4a5f7410
phurhard <phurhardeen@gmail.com>
2023-10-13 14:59:27 +0100

    Subcat

--- product_category/urls.py
+++ product_category/urls.py
@@ -3,6 +3,5 @@ from .views import ProductListByCategoryView
 
 
 urlpatterns = [
-        # re_path(r'^api/products/(?P<category>[\w\s]+)/$', ProductListByCategoryView.as_view(), name='sample'),
         path('products/<str:categories>', ProductListByCategoryView.as_view(), name='get_all_products_by_categories'),
 ]
