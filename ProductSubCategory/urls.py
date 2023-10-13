b73e02aaf235b926be88dc692ee4ee9f09c1da07
Breeze Concept <breezeconcept3@gmail.com>
2023-10-13 22:41:50 +0100

    modified product recommendation and product images

--- ProductSubCategory/urls.py
+++ ProductSubCategory/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import GetCategoryNames, GetImage, GetProductsSubCategory
+from .views import GetCategoryNames, GetImages, GetProductsSubCategory
 
 urlpatterns = [
         path('categoryNames/', GetCategoryNames.as_view(), name='category-names'),
-        path('image/<imageId>/', GetImage.as_view(), name='images'),
+        # path('image/<imageId>/', GetImage.as_view(), name='images'),
+        path('image/<productId>/', GetImages.as_view(), name='product-images'),
         path('products/<str:category>/<str:subcategory>/', GetProductsSubCategory.as_view(), name='product-sub-category'),
         # not working yet
 
