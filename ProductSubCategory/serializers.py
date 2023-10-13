4112e79cd4c36c74ec8b6a16e68b06a989926d1d
phurhard <phurhardeen@gmail.com>
2023-10-13 14:51:46 +0100

    Add sub catehories

--- ProductSubCategory/serializers.py
+++ ProductSubCategory/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from MarketPlace.models import Product
+from MarketPlace.models import Product, ProductSubCategory
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
+        fields = '__all__'
+
+class ProductsubCatSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductSubCategory
         fields = '__all__'
--- product_category/urls.py
+++ product_category/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from .views import ProductListByCategoryView
 
 
 urlpatterns = [
-    path('products/<str:categories>', ProductListByCategoryView.as_view(), name='get_all_products_by_categories'),
+        # re_path(r'^api/products/(?P<category>[\w\s]+)/$', ProductListByCategoryView.as_view(), name='sample'),
+        path('products/<str:categories>', ProductListByCategoryView.as_view(), name='get_all_products_by_categories'),
 ]
