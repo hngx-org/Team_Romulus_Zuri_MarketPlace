563e1c96355fc6b44fdc46724e6a4c7b959ae7b7
Code bee <codebee345@outlook.com>
2023-10-13 06:24:53 +0100

    Added functionality to retrurn currency symbol along with product detail

---
+++ addrecent/currencies.py
@@ -0,0 +1,31 @@
+#this file will contain currency codes and their corresponding currency symbol
+
+currency_data = {
+    'USD': '$',  # United States Dollar
+    'EUR': '€',  # Euro
+    'GBP': '£',  # British Pound Sterling
+    'JPY': '¥',  # Japanese Yen
+    'AUD': 'A$',  # Australian Dollar
+    'CAD': 'C$',  # Canadian Dollar
+    'CHF': 'Fr',  # Swiss Franc
+    'CNY': '元',  # Chinese Yuan Renminbi
+    'SEK': 'kr',  # Swedish Krona
+    'NZD': 'NZ$',  # New Zealand Dollar
+    'INR': '₹',  # Indian Rupee
+    'SGD': 'S$',  # Singapore Dollar
+    'NGN': '₦',  # Nigerian Naira
+    'HRK': 'kn',  # Croatian Kuna
+    'CZK': 'Kč',  # Czech Koruna
+    'ISK': 'kr',  # Icelandic Króna
+    'RON': 'lei',  # Romanian Leu
+    'CHF': 'Fr',  # Swiss Franc
+    'GBP': '£',  # British Pound Sterling
+    'CAD': 'C$',  # Canadian Dollar
+    'GHS': 'GH₵',  # Ghanaian Cedi
+    'KES': 'Ksh',  # Kenyan Shilling
+    'ZAR': 'R',  # South African Rand
+    'IDR': 'Rp',  # Indonesian Rupiah
+    'KRW': '₩',  # South Korean Won
+    'THB': '฿',  # Thai Baht
+    'ETB': 'Br',  # Ethiopian Birr
+}
