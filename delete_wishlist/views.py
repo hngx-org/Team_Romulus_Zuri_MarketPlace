6c0fb879e94112137bb1fcaa73cf4754667f0246
Stoney <chisomd90@gmail.com>
2023-10-13 07:56:59 +0100

    delete_wishlist logic working fine

--- delete_wishlist/views.py
+++ delete_wishlist/views.py
@@ -1,58 +1,22 @@
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
 from rest_framework import status
-from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.exceptions import ValidationError
+from MarketPlace.models import Wishlist
 import uuid
-from .models import Wishlist
 
-
-@api_view(['DELETE'])
-@permission_classes([IsAuthenticated])
-def delete_from_wishlist(request, product_id):
-    try:
-        # Check if the product_id is a valid UUID
-        uuid.UUID(str(product_id))
-    except ValueError:
-        raise ValidationError('Invalid UUID format for product_id')
-
-    try:
-        # Check if the product exists in the wishlist of the logged-in user
-        wishlist_item = Wishlist.objects.get(user=request.user, product_id=product_id)
-    except Wishlist.DoesNotExist:
-        return Response({'message': 'Product not found in your wishlist.'}, status=status.HTTP_NOT_FOUND)
-
-    # Delete the product from the wishlist
-    wishlist_item.delete()
-
-    return Response({'message': 'Product removed from your wishlist.'}, status=status.HTTP_204_NO_CONTENT)
-
-# from rest_framework import status
-# from rest_framework.decorators import api_view, permission_classes
-# from rest_framework.response import Response
-# from rest_framework.permissions import IsAuthenticated
-# from rest_framework.exceptions import ValidationError
-# import uuid
-# from .models import Wishlist
-# from .decorators import skip_authentication  # Import the skip_authentication decorator
-
-# # Apply the skip_authentication decorator to the view
-# @api_view(['DELETE'])
-# @skip_authentication
-# def delete_from_wishlist(request, product_id):
-#     try:
-#         # Check if the product_id is a valid UUID
-#         uuid.UUID(str(product_id))
-#     except ValueError:
-#         raise ValidationError('Invalid UUID format for product_id')
-
-#     try:
-#         # Check if the product exists in the wishlist of the logged-in user
-#         wishlist_item = Wishlist.objects.get(user=request.user, product_id=product_id)
-#     except Wishlist.DoesNotExist:
-#         return Response({'message': 'Product not found in your wishlist.'}, status=status.HTTP_NOT_FOUND)
-
-#     # Delete the product from the wishlist
-#     wishlist_item.delete()
-
-#     return Response({'message': 'Product removed from your wishlist.'}, status=status.HTTP_204_NO_CONTENT)
+class DeleteWishlistItem(APIView):
+    authentication_classes = []  # Requires authentication
+    
+    def delete(self, request, product_id):
+        try:
+            product_id = uuid.UUID(product_id)  # Validate the UUID
+            item = get_object_or_404(Wishlist, product_id=product_id, user_id=request.user.id)
+            item.delete()
+            return Response({'message': 'Product removed from wishlist'}, status=status.HTTP_200_OK)
+        except ValueError:
+            return Response({'error': 'Invalid UUID format'}, status=status.HTTP_400_BAD_REQUEST)
+        except Wishlist.DoesNotExist:
+            return Response({'error': 'Product not found in wishlist'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({'error': 'Internal Server Error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
