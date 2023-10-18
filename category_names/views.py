d19ca9abd4c42afef49ba08d6010f6658236ce6e
Precious Iheanyi <emmypresh777@gmail.com>
2023-10-18 12:10:26 +0100

    added 'success' to the response

--- category_names/views.py
+++ category_names/views.py
@@ -24,8 +24,15 @@ class CategoryNameView(APIView):
                 category_serializer['subcategories'] = subcategory_data
 
                 category_data.append(category_serializer)
-
-            return Response({'message': 'Category names returned successfully', 'status': status.HTTP_200_OK, 'data': category_data})
+                
+            response = {
+                'status': status.HTTP_200_OK,
+                'success': True,
+                'message': 'Category names returned successfully',
+                'data': category_data
+            }
+            
+            return Response(response, status=status.HTTP_200_OK)
         except Exception as e:
             
             return Response({'message': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
