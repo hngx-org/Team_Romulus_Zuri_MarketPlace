2624317975a317707a6127bde254e6b04f629660
Daniel Amadi <danielamadi000@gmail.com>
2023-10-18 10:26:03 +0000

    Address not found error #153

--- product_retrieval/views.py
+++ product_retrieval/views.py
@@ -18,12 +18,12 @@ class ProductSearchView(APIView):
             )
 
             # If no products found, return an appropriate message
-            if not products.exists():
-                return Response({"status": "error", "message": "No products found matching the search term."}, status=404)
+            # if not products.exists():
+            #     return Response({"status": "error", "message": "No products found matching the search term."}, status=404)
 
             serializer = ProductSerializer(products, many=True)
             return Response({
-                "status": "success",
+                "status": "200",
                 "message": f"Found {len(serializer.data)} products matching the search term.",
                 "data": serializer.data,
             })
