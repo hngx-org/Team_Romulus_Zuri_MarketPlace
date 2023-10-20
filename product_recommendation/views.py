d4c50402bbb1ad1fd49362be3817bdba89be1761
Precious Iheanyi <emmypresh777@gmail.com>
2023-10-20 06:55:02 +0100

    improve recommendation queries

--- product_recommendation/views.py
+++ product_recommendation/views.py
@@ -14,6 +14,7 @@ from rest_framework import status
 from MarketPlace.models import Product, ProductImage, User
 # from .serializers import ProductImageSerializer, UserSerializer, ProductSerializer
 from all_products.serializers import AllProductSerializer as ProductSerializer
+from django.core.exceptions import ObjectDoesNotExist
 
 class ProductRecommendationView(APIView):
     def get(self, request):
@@ -85,36 +86,49 @@ class ProductRecommendationView(APIView):
 class SimilarProductRecommendationView(APIView):
 
     def get(self, request, product_id):
+        
         try:
             current_product = Product.objects.get(id=product_id)
+            
+            # Create a subquery to get IDs of products in the same category
+            similar_product_ids = Product.objects.filter(
+                category=current_product.category
+            ).exclude(id=product_id).values_list('id', flat=True)[:4]
 
-        except Product.DoesNotExist:
+            # Use the subquery to retrieve the similar products
+            similar_products = Product.objects.filter(id__in=similar_product_ids)
+            
+            serializer = ProductSerializer(similar_products, many=True)
+
             response_data = {
-                "status_code": 404,
-                "message": "Product not found",
+                'status_code': 200,
 
-                "status": "error",
+                'message': 'Here are similar products',
+                'data': {
+                    'similar_products': serializer.data,
+                },
+                'status': 'success',
             }
-            return Response(response_data, status=status.HTTP_404_NOT_FOUND)
 
-        similar_products = Product.objects.filter(category=current_product.category).exclude(id=product_id)
+            return Response(response_data, status=status.HTTP_200_OK)
+    
+        except ObjectDoesNotExist:
+                response_data = {
+                    'status_code': 404,
+                    'message': 'Product not found',
+                    'data': [],
+                    'status': 'error',
+                }
+                return Response(response_data, status=status.HTTP_404_NOT_FOUND)
 
-        recommended_products = similar_products[:4]
-
-        serializer = ProductSerializer(recommended_products, many=True)
-
-        response_data = {
-            "status_code": 200,
-
-            "message": "Here are similar products",
-            "data": {
-                "similar_products": serializer.data,
-            },
-            "status": "success",
-        }
-
-        return Response(response_data, status=status.HTTP_200_OK)
-
+        except Exception as e:
+            # Log the exception for debugging
+            response_data = {
+                'status': 'error',
+                'message': f'An unexpected error occurred: {str(e)}',
+                'data': {'error': str(e)}
+            }
+            return Response(response_data, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 
