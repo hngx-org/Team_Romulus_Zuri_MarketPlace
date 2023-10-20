2c7ed391aaeac5562887b6db7540adbf3b8ba4dd
Daniel Amadi <danielamadi000@gmail.com>
2023-10-20 16:42:23 +0000

    Add pagination for product-retrieval

--- core/settings.py
+++ core/settings.py
@@ -191,6 +191,10 @@ REST_FRAMEWORK = {
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     # 'PAGE_SIZE': 5
 }
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
+}
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
