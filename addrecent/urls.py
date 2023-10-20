a08f88646eda8a9ff8c84a285a8df3460b803ed5
Femi-ID <104504319+Femi-ID@users.noreply.github.com>
2023-10-20 14:55:22 +0100

    Update the url for addrecent to a standard url convention

--- addrecent/urls.py
+++ addrecent/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('add-recently-viewed/<str:user_id>/<str:product_id>/', views.CreateRecentlyViewd.as_view(), name='recently-viewed'),
-    path('getproduct/<str:id>/<str:user_id>/', views.GetProductItem.as_view(), name='get-product-item-by-id'),
+    path('get-product/<str:id>/<str:user_id>/', views.GetProductItem.as_view(), name='get-product-item-by-id'),
     path('id-list-products/', views.GetProductByIdList, name='get-product-by-id-list')
 ]
