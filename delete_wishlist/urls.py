9189d6f2e7baa45ca255761cb1d2fcc24653c5e6
chisomd90 <chisomd90@gmail.com>
2023-10-20 12:37:44 +0100

    Removed underscore from delete_wishlist endpoint uri

--- delete_wishlist/urls.py
+++ delete_wishlist/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import DeleteWishlistItem
 
 urlpatterns = [
-    path('users/<user_id>/wishlist/<product_id>/', DeleteWishlistItem.as_view(), name='delete_wishlist_item'),
+    path('users/<user-id>/wishlist/<product-id>/', DeleteWishlistItem.as_view(), name='delete_wishlist_item'),
 ]
