21d9b233155b6351534d140a79cd34bff064e819
Daniel Amadi <danielamadi000@gmail.com>
2023-10-21 06:47:12 +0000

    Add search highlighting to query

--- product_retrieval/serializers.py
+++ product_retrieval/serializers.py
@@ -2,6 +2,9 @@ from rest_framework import serializers
 from MarketPlace.models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
+    highlighted_name = serializers.CharField(read_only=True, required=False)  # added
+    highlighted_description = serializers.CharField(read_only=True, required=False)  # added
+    
     class Meta:
         model = Product
         fields = '__all__'
