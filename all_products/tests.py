acf0c2cefd6863ba6a0bbdf02d5c8eacd1d9be5b
FestusMike <festusmike98@gmail.com>
2023-10-22 13:39:59 +0100

    tests written

--- all_products/tests.py
+++ all_products/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+from MarketPlace.models import Product
 
-# Create your tests here.
+
+class ProductListAPITest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_list_products(self):
+        Product.objects.create(name="Product1", price=10.0)
+        Product.objects.create(name="Product2", price=20.0)
+        response = self.client.get('https://staging.zuri.team/api/marketplace/v1/product-list/')  
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['data']), 2)  
+        
+
+    def test_list_products_pagination(self):
+        # We will Create a large number of products to test pagination
+        for i in range(15):
+            Product.objects.create(name=f"Product{i}", price=10.0)
+        response = self.client.get('https://staging.zuri.team/api/marketplace/v1/product-list/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['data']), 10) 
+        self.assertTrue(response.data['page_info']['next'])
+        
+
+    def test_list_products_no_data(self):
+        response = self.client.get('https://staging.zuri.team/api/marketplace/v1/product-list/')  
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['data']), 0)  
+        
+
+    
+
