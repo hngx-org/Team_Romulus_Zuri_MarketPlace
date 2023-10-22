1617381532cd64bd385648ace5769cfcfd871afb
Daniel Amadi <danielamadi000@gmail.com>
2023-10-22 07:14:59 +0000

    Improve exception handling

--- product_retrieval/views.py
+++ product_retrieval/views.py
@@ -1,9 +1,10 @@
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Q, Value, CharField
 from django.db.models.functions import Replace, Concat
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.throttling import UserRateThrottle
+from rest_framework.exceptions import ParseError, ValidationError
 from MarketPlace.models import Product
 from all_products.serializers import AllProductSerializer as ProductSerializer
 
@@ -21,7 +22,7 @@ class ProductSearchView(APIView):
         """
         query = request.query_params.get('search', None)
         if not query:
-            return Response({"status": "error", "message": "A search term must be provided."}, status=400)
+            raise ParseError({"status": "error", "message": "A search term must be provided.", "status_code": 400})
 
         try:
             # Highlighting logic
@@ -38,7 +39,14 @@ class ProductSearchView(APIView):
             # Manual Pagination
             page = request.query_params.get('page', 1)
             paginator = Paginator(products, 10)  # 10 items per page
-            current_page = paginator.get_page(page)
+            try:
+                current_page = paginator.page(page)
+            except PageNotAnInteger:
+                # If page is not an integer, deliver first page.
+                current_page = paginator.page(1)
+            except EmptyPage:
+                # If page is out of range, deliver last page of results.
+                current_page = paginator.page(paginator.num_pages)
 
             serializer = ProductSerializer(current_page, many=True)
 
@@ -57,5 +65,7 @@ class ProductSearchView(APIView):
 
             return Response(response_data)
 
+        except ValueError as e:
+            raise ValidationError(f"Error processing request: {str(e)}")
         except Exception as e:
             return Response({"status": "error", "message": f"An unexpected error occurred: {str(e)}"}, status=500)
