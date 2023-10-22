5753d4ce758ce5adc06407a44fd7e7a4f63975c0
chisomd90 <chisomd90@gmail.com>
2023-10-22 21:11:37 +0100

    Created a new endpoint that deletes all user's wishlist item

--- delete_all_wishlist/models.py
+++ delete_all_wishlist/models.py
@@ -1,6 +1,6 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 class Wishlist(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product_id = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='delete_all_wishlist_set')
+    # ... other fields ...
